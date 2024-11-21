008f489ca7d7c269ab6a8f2192114e8354c963d3
Ching Jui Young <young.chingjui@gmail.com>
2024-11-21 10:34:54 +0800

    updated theming

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,9 @@ export default async function PortfolioPage() {
   const data = await fetchData()
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300">
+    <div className="min-h-screen text-primary-foreground bg-gradient-to-br from-teal-800 via-teal-500 to-blue-300">
       <header className="container mx-auto pt-52 pb-6 px-4 bg-opacity-80 backdrop-blur-sm">
-        <h1 className="text-5xl font-bold text-gray-800 font-rokkitt">
-          Young & AI
-        </h1>
+        <h1 className="text-5xl font-bold font-rokkitt">Young & AI</h1>
       </header>
       <main className="container mx-auto px-4 py-8">
         {data.map((section, index) => (
--- components/Section.tsx
+++ components/Section.tsx
@@ -4,30 +4,21 @@ import Link from "next/link"
 
 export default function Section({ data }: { data: SectionData }) {
   const displayItems = data.items.slice(0, 5)
-  const hasMoreItems = data.items.length > 5
 
   return (
     <section className="mb-12">
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-semibold text-gray-700">{data.title}</h2>
-        {hasMoreItems && (
-          <Link
-            href={`/${data.title.toLowerCase().replace(/\s+/g, "-")}`}
-            passHref
-          >
-            <Button variant="outline" size="sm">
-              Show more
-            </Button>
-          </Link>
-        )}
+        <h2 className="text-2xl font-semibold">{data.title}</h2>
       </div>
       <div className="relative">
         <div className="flex overflow-x-auto space-x-4 pb-4 scrollbar-hide">
           {displayItems.map((item) => (
             <Link key={item.id} href={`/client/${item.slug}`} passHref>
-              <div className="flex-none w-64 bg-white p-4 rounded-lg shadow-md cursor-pointer transition-transform hover:scale-105">
-                <h3 className="font-medium text-lg mb-2">{item.title}</h3>
-                <p className="text-sm text-gray-600 truncate">
+              <div className="flex-none w-64 bg-card p-4 rounded-lg shadow-md cursor-pointer transition-transform hover:scale-105">
+                <h3 className="font-medium text-lg mb-2 text-card-foreground">
+                  {item.title}
+                </h3>
+                <p className="text-sm truncate text-muted-foreground">
                   {item.description}
                 </p>
               </div>
