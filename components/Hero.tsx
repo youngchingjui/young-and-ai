a52308e1ad4654b880cda109f5dade6657523589
Ching Jui Young <young.chingjui@gmail.com>
2025-08-19 12:14:45 +0800

    cleanpu

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,13 @@
 export default function Hero() {
   return (
     <section className="relative min-h-[90dvh] grid place-items-center px-4">
-      <p className="absolute left-4 top-6 md:left-8 md:top-8 text-base md:text-lg">Young & AI</p>
-      <h1 className="font-extrabold tracking-[-0.02em] leading-[0.82] text-center text-[clamp(2.5rem,25vmin,20rem)]">
+      <div className="absolute w-full  top-16 md:left-8 md:top-16 text-2xl md:text-3xl text-center">
+        <p className="text-2xl md:text-3xl">Young & AI</p>
+      </div>
+      <h1 className="font-extrabold tracking-[-0.02em] leading-[0.82] text-center text-[clamp(2.5rem,20vmin,12rem)]">
         <span className="block">WE</span>
         <span className="block">BUILD</span>
-        <span className="block">AI</span>
+        <span className="block text-primary text-[clamp(2.5rem,50vmin,30rem)]">AI</span>
       </h1>
     </section>
   )
